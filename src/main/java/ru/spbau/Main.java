ca1eb04b23bfcf65cd1e9d91ac7889d836e7e679
AirVan21 <airvan21@gmail.com>
2016-02-22 23:35:11 +0300

    Using Stanford NER for geo-location handling

--- src/main/java/ru/spbau/Main.java
+++ src/main/java/ru/spbau/Main.java
@@ -1,9 +1,10 @@
 package ru.spbau;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
+import edu.stanford.nlp.ling.CoreLabel;
 import org.ahocorasick.trie.Emit;
 import ru.spbau.epubParser.EPUBHandler;
 import ru.spbau.jsonParser.JSONHandler;
+import ru.spbau.nerWrapper.NERWrapper;
 import ru.spbau.trieAhoCorasick.TrieWrapper;
 
 import java.io.*;
@@ -11,12 +12,23 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String serializedClassifier = "./data/classifiers/english.all.3class.distsim.crf.ser.gz";
         String jsonPath = "./data/countriesToCitiesFile.json";
         String bookPath = "./data/IAmLegend.epub";
 
         Map<String, List<String>> cityData = JSONHandler.readFromPath(jsonPath);
         String book = EPUBHandler.readFromPath(bookPath);
         Collection<Emit> entires = TrieWrapper.buildTrie(cityData).parseText(book);
+
+
+        NERWrapper locationNER = new NERWrapper(serializedClassifier);
+        Map<String, List<CoreLabel>> locationMap = locationNER.classifyBook(book);
+
+        for (String label : locationMap.keySet()) {
+            System.out.print(label + " -> ");
+            System.out.println(locationMap.get(label));
+        }
+
     }
 }
---
+++ src/main/java/ru/spbau/nerWrapper/NERWrapper.java
@@ -0,0 +1,68 @@
+package ru.spbau.nerWrapper;
+
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.crf.CRFClassifier;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Named Entity Recognizer Wrapper
+ */
+public class NERWrapper {
+    private AbstractSequenceClassifier<CoreLabel> classifier;
+    private final static String searchTag = "LOCATION";
+
+    /**
+     * Exception throwing constructor. Rewrite?
+     */
+    public NERWrapper (String pathToClassifier) throws IOException, ClassNotFoundException {
+        classifier = CRFClassifier.getClassifier(pathToClassifier);
+    }
+
+    public Map<String, List<CoreLabel>> classifyBook(String pathToBook) {
+        List<List<CoreLabel>> taggedText = classifier.classify(pathToBook);
+        Map<String, List<CoreLabel>> locationMap = new HashMap<String, List<CoreLabel>>();
+
+        for (List<CoreLabel> sentence : taggedText) {
+            handleLocation(sentence, locationMap);
+        }
+
+        return locationMap;
+    }
+
+    /**
+     * Retrieves if sentence has geo-location tag.
+     * Multi-word get-locations will be merged in one string.
+     *
+     * @param sentence
+     * @param locationMap
+     */
+    private void handleLocation(List<CoreLabel> sentence, Map<String, List<CoreLabel>> locationMap) {
+        Iterator<CoreLabel> sentenceIterator = sentence.iterator();
+
+        while (sentenceIterator.hasNext()) {
+            CoreLabel item = sentenceIterator.next();
+
+            if (item.get(CoreAnnotations.AnswerAnnotation.class).equals(searchTag)) {
+                String locationName = item.value();
+                // Handle multi-word location
+                while (sentenceIterator.hasNext()) {
+                    item = sentenceIterator.next();
+
+                    if (item.get(CoreAnnotations.AnswerAnnotation.class).equals(searchTag)) {
+                        locationName += " " + item.value();
+                    } else {
+                        break; // Geo-location ended
+                    }
+                }
+                locationMap.put(locationName, sentence);
+            }
+        }
+    }
+}
